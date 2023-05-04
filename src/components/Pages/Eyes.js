b12c8de3ff2f50328f39e32edf0f5cc6ea251a60
christinelin <christinelin.7771@gmail.com>
2023-05-04 13:45:59 -0400

    Messaging

--- src/components/Pages/Eyes.js
+++ src/components/Pages/Eyes.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import { ProjectList } from '../../data/ProjectList'
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+} from "@chatscope/chat-ui-kit-react";
 
 import './Projects.css'
 
@@ -7,6 +15,21 @@ const Eyes = () => {
   return (
     <div className = "Eyes">
       <h1>Angel in Others' Eyes</h1>
+      <div style={{ position: "relative", height: "300px" , width: "75%"}}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "just now",
+                sender: "Joe",
+              }}
+            />
+          </MessageList>
+        </ChatContainer>
+      </MainContainer>
+    </div>
     </div>
   )
 }
