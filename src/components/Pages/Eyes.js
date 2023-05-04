29c6ae8618aff25f6c2cf7e21c68031af1a32388
christinelin <christinelin.7771@gmail.com>
2023-05-04 14:01:54 -0400

    React-Chat-UI

--- src/components/Pages/Eyes.js
+++ src/components/Pages/Eyes.js
@@ -1,34 +1,51 @@
 import React from 'react'
 import { ProjectList } from '../../data/ProjectList'
-import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import {
-  MainContainer,
-  ChatContainer,
-  MessageList,
-  Message,
-  MessageInput,
-} from "@chatscope/chat-ui-kit-react";
+// import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+// import {
+//   MainContainer,
+//   ChatContainer,
+//   MessageList,
+//   Message,
+//   MessageInput,
+// } from "@chatscope/chat-ui-kit-react";
+import { ChatFeed, ChatBubble, BubbleGroup, Message } from "react-chat-ui";
 
 import './Projects.css'
 
 const Eyes = () => {
+  const allMessage = [
+    {
+      message: "Hello",
+      senderName: "Eve"
+    }
+  ]
+   
+
+
   return (
     <div className = "Eyes">
       <h1>Angel in Others' Eyes</h1>
-      <div style={{ position: "relative", height: "300px" , width: "75%"}}>
-      <MainContainer>
-        <ChatContainer>
-          <MessageList>
-            <Message
-              model={{
-                message: "Hello my friend",
-                sentTime: "just now",
-                sender: "Joe",
-              }}
-            />
-          </MessageList>
-        </ChatContainer>
-      </MainContainer>
+    <div>
+    
+    <ChatFeed
+      messages={allMessage}
+      showSenderName
+
+      bubbleStyles={
+        {
+          text: {
+            fontSize: 30
+          },
+          chatbubble: {
+            borderRadius: 50,
+            padding: 10
+          }
+        }
+      }
+    >
+
+    </ChatFeed>
+    
     </div>
     </div>
   )
